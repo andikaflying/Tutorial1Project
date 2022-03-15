1d251a3b71ee3591db36a46750d341bb9be56d44
Andika Kurniawan <andikaflying@gmail.com>
2022-03-15 21:14:17 +0700

    call api

--- src/pages/AxiosPage/index.js
+++ src/pages/AxiosPage/index.js
@@ -1,10 +1,12 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TextInput, Pressable} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import {onChange} from 'react-native-reanimated';
 
 const AxiosPage = () => {
   const [informasiGempa, setInformasiGempa] = useState(null);
   const [todoList, setTodoList] = useState(null);
+  const [input, setInput] = useState(null);
 
   const displayData = () => {
     return axios
@@ -25,7 +27,59 @@ const AxiosPage = () => {
       });
   };
 
+  const onKirimData = () => {
+    const sentObject = {
+      title: input,
+      description: input,
+      status: 'PENDING',
+    };
+    return axios
+      .post('http://code.aldipee.com/api/v2/todos', sentObject)
+      .then(function (response) {
+        if (response.data.success) {
+          const id = response.data.data.id;
+          console.log('Id = ' + id);
+          update(id);
+        }
+      });
+  };
+
+  const update = async id => {
+    const data = {
+      title: 'Bernyanyi ddddeee',
+      description: 'Bernyanyi sdsdsds',
+      status: 'DONE',
+    };
+
+    return await axios
+      .patch('http://code.aldipee.com/api/v2/todos/' + id, data)
+      .then(function (response) {
+        console.log('Update : ' + JSON.stringify(response.data.success));
+        deleteTodo(id);
+      });
+  };
+
+  const deleteTodo = async id => {
+    return await axios
+      .delete('http://code.aldipee.com/api/v2/todos/' + id)
+      .then(function (response) {
+        console.log('Delete : ' + JSON.stringify(response));
+      });
+  };
+
+  const onChangeText = value => {
+    console.log('Value = ' + value);
+    setInput(value);
+  };
+
+  const onClickButton = () => {
+    onKirimData();
+  };
+
   useEffect(() => {
+    // test();
+    // update();
+    // deleteTodo();
     displayData();
     displayTodoList();
   }, []);
@@ -43,6 +97,16 @@ const AxiosPage = () => {
         </>
       )}
       <View style={styles.todoList}>
+        <View style={styles.inputForm}>
+          <TextInput
+            style={styles.input}
+            placeholder="title"
+            onChangeText={onChangeText}
+          />
+          <Pressable style={styles.button} onPress={onClickButton}>
+            <Text style={styles.buttonText}>Buat baru</Text>
+          </Pressable>
+        </View>
         {todoList != null &&
           todoList.map(item => {
             return (
@@ -80,5 +144,26 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     color: 'black',
     fontWeight: '400',
+  },
+  inputForm: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: 'red',
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    marginLeft: 20,
+  },
+  buttonText: {
+    color: 'white',
   },
 });
